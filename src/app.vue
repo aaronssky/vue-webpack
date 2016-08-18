ebd4b0bc6f464bf4a766fdd39a6a05245a07050a
chenjunlun <chenjunlun@yaochufa.com>
2016-08-18 19:02:45 +0800

    add files

---
+++ src/app.vue
@@ -0,0 +1,19 @@
+<template>
+<div class="message">{{ msg }}</div>
+</template>
+
+<script>
+export default {
+ data () {
+ return {
+ msg: 'Hello from vue-loader!111'
+ }
+ }
+}
+</script>
+
+<style>
+.message {
+ color: blue;
+}
+</style>
